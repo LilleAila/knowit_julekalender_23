19b099983bf8de450cdd89c8197cc6a47026fa51
LilleAila <olai.solsvik@gmail.com>
2023-12-04 12:05:34 +0000

    Luke 4

---
+++ luke4/target/debug/deps/luke4-198bacd29ccb0514.d
@@ -0,0 +1,5 @@
+/home/alarm/Desktop/knowit_julekalender_23/luke4/target/debug/deps/luke4-198bacd29ccb0514: src/main.rs
+
+/home/alarm/Desktop/knowit_julekalender_23/luke4/target/debug/deps/luke4-198bacd29ccb0514.d: src/main.rs
+
+src/main.rs:
---
+++ luke4/target/debug/luke4.d
@@ -0,0 +1,1 @@
+/home/alarm/Desktop/knowit_julekalender_23/luke4/target/debug/luke4: /home/alarm/Desktop/knowit_julekalender_23/luke4/src/main.rs
