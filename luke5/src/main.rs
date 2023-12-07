c17329455e018882e3ab5dbe63c7a19698c53d52
LilleAila <olai.solsvik@gmail.com>
2023-12-07 11:33:55 +0100

    Luke 7

--- luke5/src/main.rs
+++ luke5/src/main.rs
@@ -1,60 +1,44 @@
-// use std::fs;
-
-fn get_sum_div(num: i32) -> i32 {
-    let num_sum: i32 = num.to_string()
+fn get_sum_div(num: f64) -> f64 {
+    let num_sum: f64 = num.to_string()
         .chars()
         .filter_map(|c| c.to_digit(10))
-        .sum::<u32>() as i32;
+        .sum::<u32>() as f64;
 
-    let divided: f32 = num as f32 / num_sum as f32;
+    let divided: f64 = num / num_sum;
 
     // println!("{} {} {}", num, num_sum, divided);
 
-    if divided % 1.0 > 0.0 { return -1 }
+    if divided % 1.0 > 0.0 { return -1.0 }
 
-    return divided as i32;
+    return divided;
 }
 
-// fn is_prime(n: i32) -> bool {
-//     if n <= 1 { // Hvis 1 er et primtall, det er det sikkert ikke
-//         return false;
-//     }
-//     if n <= 3 {
-//         return true;
-//     }
-//     if n % 2 == 0 || n % 3 == 0 {
-//         return false;
-//     }
-//
-//     let mut i = 5;
-//     while i * i <= n {
-//         if n % i == 0 || n % (i + 2) == 0 {
-//             return false;
-//         }
-//         i += 6;
-//     }
-//
-//     true
-// }
+fn is_prime(n: f64) -> bool {
+    if n <= 1.0{
+        return false;
+    }
+    if n <= 3.0 {
+        return true;
+    }
+    if n % 2.0 == 0.0 || n % 3.0 == 0.0 {
+        return false;
+    }
 
-fn is_prime (n: i32) -> bool {
-    if n == 2 { return true }
-    if n % 2 == 0 { return false }
-    let mut i = 3;
+    let mut i = 5.0;
     while i * i <= n {
-        if n % i == 0 {
+        if n % i == 0.0 || n % (i + 2.0) == 0.0 {
             return false;
         }
-        i += 2;
+        i += 6.0;
     }
+
     true
-    // !(2..(n as f32).sqrt().ceil() as i32).any(|a| n % a == 0)
 }
 
-fn check_prime_div(num: i32) -> bool {
-    let sum_div: i32 = get_sum_div(num);
-    if sum_div < 0 { return false };
-    return is_prime(sum_div);
+fn check_prime_div(num: f64) -> bool {
+    let sum_div: f64 = get_sum_div(num);
+    if sum_div < 0.0 { return false };
+    return is_prime(sum_div as f64);
 }
 
 fn main() {
@@ -63,7 +47,7 @@ fn main() {
     // let max_num: i32 = 100;
     let mut num_primes: i32 = 0;
     for i in (min_num)..(max_num + 1) {
-        let checked: bool = check_prime_div(i);
+        let checked: bool = check_prime_div(i as f64);
         if checked {
             num_primes += 1;
             // println!("{}", i);
@@ -73,8 +57,17 @@ fn main() {
 
     println!("Antall sum-dele-primtall: {}", num_primes);
     // 6k±1 prime alg: 1020748
+    // primes library: 1020748 <--
     // One-line from SO: 1022242
     // Simple: 1020757
+    // 2 is not prime: 1020747
+    // 1 is prime: 1020757
+    // Rounded: 7182262
+    // Ceiled: 7235739
+    // Floored: 7233747
+    // Without decimal check: 7233747
+    // Change everything to use flats: 642344 (correct)
 
     // println!("{}", check_prime_div(9015));
+    // println!("{}", check_prime_div(18));
 }
